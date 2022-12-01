eba43011bf529cccdf0189e5f4d63cda4d623d7e
activecodar <activecodar@gmail.com>
2022-12-01 17:40:17 +0000

    calc
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import argparse
+from calculator_1 import add, sub, mul, div
+
+
+def calculate(args):
+    matches = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+    a,o,b = int(args[0]), args[1], int(args[2])
+    if len(args) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        return 1
+    if o not in ('+', '-', '*', '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        return 1
+    else:
+        res = matches[o](a, b)
+        print("{:d} {} {:d} = {:d}".format(a, o, b, res))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_args', nargs='*')
+    args = parser.parse_args()._args
+    calculate(args)
+    
