d65be548a039f0ff1f1e4c610031bcd991a9d12b
activecodar <activecodar@gmail.com>
2022-12-01 12:55:10 +0000

    fix failing checks
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import argparse
+
+
+def output(args, plural=False):
+    plural_letter = 's' if plural else ''
+    if len(args) == 0:
+        print("{:d} argument{}.".format(len(args), plural_letter))
+    else:
+        print("{:d} argument{}:".format(len(args), plural_letter))
+        for idx, arg in enumerate(args):
+            print("{:d}: {}".format(idx + 1, arg))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_args', nargs='*')
+    args = parser.parse_args()._args
+    output(args, plural=len(args) != 1)
