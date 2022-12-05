c6e5fea3697c15d7c50df4740f0b182f76ab7711
activecodar <activecodar@gmail.com>
2022-12-05 09:03:26 -0800

    Lists of lists = Matrix
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        _rw = ' '.join(str(x) for x in row)
-        print("{}".format(_rw))
+        print(" ".join("{:d}".format(x) for x in row))
