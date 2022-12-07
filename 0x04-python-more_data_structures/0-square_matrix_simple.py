52b2620579ae6c4d4ad67155a9eb6e5f007c7182
activecodar <activecodar@gmail.com>
2022-12-07 08:34:25 -0800

    square matrix
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    squared = [[0 for col in range(len(matrix[0]))] 
+              for row in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            squared[i][j] = matrix[i][j] ** 2
+    return squared
