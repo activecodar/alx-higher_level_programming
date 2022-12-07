2d057b39598f1e9020cefe2d6e049ae3e2be76ec
activecodar <activecodar@gmail.com>
2022-12-07 10:21:15 -0800

    complex delete
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys = list(a_dictionary.keys())
+    for key in keys:
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
