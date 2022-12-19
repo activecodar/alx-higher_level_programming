6f6d3e20ca88d43d46ff78b61b923b9000f73784
activecodar <activecodar@gmail.com>
2022-12-19 18:19:31 +0000

    2. Print and count integers
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    elements_printed = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                elements_printed += 1
+        print()
+    except IndexError:
+        print()
+        pass
+    return elements_printed
