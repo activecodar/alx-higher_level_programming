3164767411207f61c509668344a136735c94a118
activecodar <activecodar@gmail.com>
2022-12-20 06:49:10 +0000

    6. Raise a message
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end='')
                 elements_printed += 1
         print()
-    except IndexError:
+    except TypeError:
         print()
         pass
     return elements_printed
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -2,6 +2,3 @@
 
 def raise_exception():
     raise TypeError()
-
-if __name__ == "__main__":
-    raise_exception()
