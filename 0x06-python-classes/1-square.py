ddf3f91246db4ed7a7467092a9a4348769a83c02
activecodar <activecodar@gmail.com>
2022-12-20 07:46:45 +0000

    1. Square with size
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains a class for representing squares in two-dimensional space.
+The Square class provides methods for calculating the area and perimeter
+of a square,and has an attribute for storing the side length of the square.
+"""
+
+
+class Square:
+    """
+    A square is a polygon with four equal sides and four right angles.
+
+    This class represents a square in two-dimensional space.
+    Attributes:
+        side_length (float): The length of a side of the square.
+    """
+    def __init__(self, size: float) -> None:
+        """Initialize a Square object.
+
+        Args:
+            size (float): The length of a side of the square.
+        """
+        self.__size = size
