6d78aff800af87f688618f55be770ed78a009f8d
activecodar <activecodar@gmail.com>
2023-01-03 17:42:07 +0000

    create rectangle
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module contains the `Rectangle` class, which represents a rectangle
+with a width and a height.
+"""
+
+
+class Rectangle:
+    """
+    A class representing a rectangle with a width and a height.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+
+    pass
