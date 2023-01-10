be6be11c45583439d09cb84bb014052f8a229f0d
activecodar <activecodar@gmail.com>
2023-01-10 02:57:15 +0000

    geometry more
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module contains a base class for
+geometry objects.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry objects.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry object.
+
+        Raises:
+            NotImplementedError: If the area is not
+            implemented for the specific geometry object.
+        """
+        raise NotImplementedError("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validate a given value.
+
+        Args:
+            name (str): The name of the value to validate.
+            value: The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
