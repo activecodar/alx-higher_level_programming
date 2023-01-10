9c7c27b158c741f3b6d4eedff821dcfb7f7c0bdf
activecodar <activecodar@gmail.com>
2023-01-10 02:47:27 +0000

    4. Only sub class of
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function that checks if an object is
+an instance of a class that inherited from a specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of
+    a class that inherited from a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check inheritance from.
+
+    Returns:
+        bool: True if the object is an instance of
+        a class that inherited from the specified class, False otherwise.
+    """
+    return isinstance(obj, a_class) or issubclass(obj.__class__, a_class)
