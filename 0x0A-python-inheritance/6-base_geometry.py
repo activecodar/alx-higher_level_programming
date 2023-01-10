5b956526b0a0f9c8369bc11f00e44e3d9bc3ccf3
activecodar <activecodar@gmail.com>
2023-01-10 02:52:16 +0000

    6. Improve Geometry
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a base class for
+geometry objects.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry objects.
+    """
+
+    def area(self):
+        """
+        Calculate the area of the geometry object.
+
+        Raises:
+            NotImplementedError: If the area is
+            not implemented for the specific geometry object.
+        """
+        raise NotImplementedError("area() is not implemented")
