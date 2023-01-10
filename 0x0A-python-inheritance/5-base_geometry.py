3e37038416cf2d1881939a7756bcf3e96faa7532
activecodar <activecodar@gmail.com>
2023-01-10 02:49:43 +0000

    5. Geometry module
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a base class for
+geometry objects.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry objects.
+
+    This class provides a base for geometry objects.
+    It has no implementation and is intended to be subclassed.
+    """
+    pass
