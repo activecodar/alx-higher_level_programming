45df514a21c9fac667c1a65383ca2a708614b4f7
activecodar <activecodar@gmail.com>
2023-01-19 14:33:43 +0000

    16. JSON string to file
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,16 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save json to file
+        """
+        filename = f"{cls.__name__}.json"
+        list_dicts = []
+        if list_objs is not None:
+            list_dictionaries = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dictionaries)
+        with open(filename, "w") as f:
+            f.write(json_string)
