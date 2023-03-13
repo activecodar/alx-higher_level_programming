eb800e48cd328960ab0cbe0d60dec5facd64271f
activecodar <activecodar@gmail.com>
2023-03-13 21:35:30 +0300

    13. Add file
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,8 @@ const args = process.argv;
 if (args.length - 2 <= 1) {
   console.log(0);
 } else {
-  const valsArr = [];
-  args.slice(2).forEach(element => {
-    valsArr.push(parseInt(element));
-  });
-  valsArr.sort().reverse();
-  console.log(valsArr[1]);
+  const intArgs = args.slice(2).map(arg => parseInt(arg));
+  const sortedInts = intArgs.sort((a, b) => b - a);
+  const secondLargest = sortedInts[1];
+  console.log(secondLargest);
 }
