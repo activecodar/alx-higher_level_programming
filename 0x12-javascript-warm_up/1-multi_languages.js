a5163c1286bab9494df9b4ff012be5c7980ccac8
activecodar <activecodar@gmail.com>
2023-03-13 20:09:48 +0300

    1. 3 languages
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+console.log(myVar);
+const myVar2 = 'Python is cool';
+console.log(myVar2);
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar3);
