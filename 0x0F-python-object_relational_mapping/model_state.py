5f827de0c9f9018c1846bafaf1345cfa4be52432
activecodar <activecodar@gmail.com>
2023-03-18 14:08:44 +0300

    6. First state model
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Create a State model that inherits from
+Base and links to the MySQL table states.
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Represents a state table for a MySQL database.
+    __tablename__ (str): The name of table.
+    id (sqlalchemy.Integer): The state's id and primary key.
+    name (sqlalchemy.String): The state's name and cannot be null.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
