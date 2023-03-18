a8dd9efdaec94e1bd2dca1f83f49cf5e046c8289
activecodar <activecodar@gmail.com>
2023-03-18 12:01:49 +0300

    1. Filter states
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,11 +16,60 @@ import sys
 import MySQLdb
 
 
-def setup_db(user, password, database, host='localhost'):
+class DBConnector:
+    """
+    A class for creating a MySQL database
+    connection and executing queries.
+    """
+
+    def __init__(self, username, password, database, host='localhost'):
+        """
+        Constructs a new DBConnector instance.
+
+        Args:
+            username: MySQL username
+            password: MySQL password
+            database: MySQL database name
+            host: MySQL host name (default 'localhost')
+        """
+        self.username = username
+        self.password = password
+        self.database = database
+        self.host = host
+
+    def db_cursor(self):
+        """
+        Cursor object to execute
+        queries on the MySQL database.
+
+        Returns:
+            A MySQLdb cursor object.
+        """
+        db = MySQLdb.connect(user=self.username,
+                             password=self.password,
+                             database=self.database,
+                             host=self.host)
+        return db.cursor()
+
+    def db_query(self, sql_string):
+        """
+        Executes a raw query on the MySQL database and returns the rows.
+        Args:
+                sql_string: raw query to execute.
+        Returns:
+                All rows affected.
+        """
+        cursor = self.db_cursor()
+        cursor.execute(sql_string)
+        return cursor.fetchall()
+
+
+def main(user, password, database, host='localhost'):
     """
         Sets up a MySQL database connection and tests the
         connection by executing a query
-        on the 'states' table.
+        on the 'states' table fetching all states
+        and ordering them in ascending order by id.
 
         Args:
             user: MySQL username
@@ -28,11 +77,11 @@ def setup_db(user, password, database, host='localhost'):
             database: MySQL database name
             host: MySQL host name (default 'localhost')
         """
-    db = MySQLdb.connect(host=host, user=user, password=password,
-                         database=database)
-    cursor = db.cursor()
-    cursor.execute('SELECT * FROM states ORDER BY id ASC;')
-    results = cursor.fetchall()
+    db_instance = DBConnector(username=user,
+                              password=password,
+                              database=database,
+                              host=host)
+    results = db_instance.db_query('SELECT * FROM states ORDER BY id ASC;')
     for row in results:
         print(row)
 
@@ -41,4 +90,4 @@ if __name__ == '__main__':
     user_input = sys.argv[1]
     password_input = sys.argv[2]
     database_input = sys.argv[3]
-    setup_db(user_input, password_input, database_input)
+    main(user_input, password_input, database_input)
