32f88a946c3875156bd6911415f07545cafa33cc
activecodar <activecodar@gmail.com>
2023-03-18 11:54:58 +0300

    0. Get all states
    Signed-off-by: activecodar <activecodar@gmail.com>

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This script sets up a MySQL database connection using the provided
-credentials and tests the connection by executing a query on the 'states' table.
+credentials and tests the connection by executing
+a query on the 'states' table.
 
 Usage:
     ./script.py <mysql username> <mysql password> <mysql database>
@@ -17,7 +18,8 @@ import MySQLdb
 
 def setup_db(user, password, database, host='localhost'):
     """
-        Sets up a MySQL database connection and tests the connection by executing a query
+        Sets up a MySQL database connection and tests the
+        connection by executing a query
         on the 'states' table.
 
         Args:
@@ -26,7 +28,8 @@ def setup_db(user, password, database, host='localhost'):
             database: MySQL database name
             host: MySQL host name (default 'localhost')
         """
-    db = MySQLdb.connect(host=host, user=user, password=password, database=database)
+    db = MySQLdb.connect(host=host, user=user, password=password,
+                         database=database)
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY id ASC;')
     results = cursor.fetchall()
