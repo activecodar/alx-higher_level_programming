8967aa61a70f5ecafc14e3639d1250a3f2b787f3
activecodar <activecodar@gmail.com>
2023-03-30 10:50:33 +0300

    script that takes in a URL, sends a GET request to the URL
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Send GET request and save response body to a variable
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+if [ "$response" -eq 200 ]; then
+  body=$(curl -s "$1")
+  echo "$body"
+else
+  echo "Error: Status code $response"
+  exit 1
+fi
