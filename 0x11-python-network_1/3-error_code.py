4da0ff2b751a30fe1df5ba1c0bc54305333dac18
activecodar <activecodar@gmail.com>
2023-04-03 21:57:13 +0300

    url that catches error and retunr code
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module handles HTTP error code
+"""
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
